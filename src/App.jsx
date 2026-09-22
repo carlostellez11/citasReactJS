bf7fb6a2d6b8c917906d0ee56d8afb52a83358b3
Carlos Téllez Sandoval <st4502@utr.edu.mx>
2026-09-22 12:11:52 -0600

    Header and buttom structure. Create modal Formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-function App() {
+import { useState } from 'react';
+import Formulario from './components/Formulario.jsx';
+import './css/main.css';
 
+function App() {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
-    <>
-      <h1>Hola Mundo!</h1>
-      <p>Primer componente con React y Vite</p>
-    </>
+    <main className="container">
+      <h1 className='titulo'>
+        Administrador de Citas <span className='titulo-bold'>Veterinario</span>
+      </h1>
+      <button
+      className='btn-nueva-cita'
+      onClick={() => setModalVisible(true)}
+      >
+        <span className='btn-texto-nueva-cita'>Nueva Cita</span>
+      </button>
+
+      {modalVisible && (
+        <div className='modal-overlay' role='dialog' aria-modal='true'>
+        <div className='modal-content'>
+          <Formulario />
+        </div>
+      </div>
+      )}
+
+    </main>
   )
 }
 
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+
+const Formulario = () => {
+    return (
+        <h1>Hola Mundo Carlos Téllez</h1>
+    );
+};
+
+export default Formulario;
